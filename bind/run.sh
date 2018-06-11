ca0ba61cdf29c009291686fc07f1216c23ac01fb
Kyng <elroy@live.be>
2018-06-11 06:18:17 +0200

    ADDR update

--- bind/run.sh
+++ bind/run.sh
@@ -2,7 +2,7 @@
 
 docker run -itd --name bind \
  -p 54:54/udp -p 953:953 -p 10000:10000 \ #changer 53 en 54 sur Ubuntu 17.04+, 53 étant pris par l'OS
- -e ROOT_PASSWORD="batte564" \
+ -e ROOT_PASSWORD="wtadmin2018" \
  -v /home/chris/wt_leroy/bind/data:/data \
  -v /home/chris/wt_leroy/bind/log:/var/log/bind \
  sameersbn/bind:latest
