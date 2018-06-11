3f8b13add133fe07ec1b475a57174dd4d96e8549
Kyng <elroy@live.be>
2018-06-11 07:24:29 +0200

    ADDR update

--- reverse-proxy/run.sh
+++ reverse-proxy/run.sh
@@ -3,8 +3,8 @@
 docker run -it --name reverse-proxy \
  --network intern --ip 10.20.30.100 \
  -p 80:80 -p 8080:8080 -p 443:443 -p 3306:3306 \
- -v /home/chris/wt_leroy/reverse-proxy/etc:/etc/nginx \
- -v /home/chris/wt_leroy/reverse-proxy/log:/var/log/nginx \
- -v /home/chris/wt_leroy/reverse-proxy/www:/var/www \
- -v /home/chris/wt_leroy/reverse-proxy/auth:/auth \
+ -v /home/chris/wt-leroy/reverse-proxy/etc:/etc/nginx \
+ -v /home/chris/wt-leroy/reverse-proxy/log:/var/log/nginx \
+ -v /home/chris/wt-leroy/reverse-proxy/www:/var/www \
+ -v /home/chris/wt-leroy/reverse-proxy/auth:/auth \
  nginx:latest 
--- web/run.sh
+++ web/run.sh
@@ -2,8 +2,8 @@
 
 docker run -itd --name web \
  --net intern --ip 10.20.30.50 \
- -v /home/chris/wt_leroy/web/www:/var/www \
- -v /home/chris/wt_leroy/web/etc:/etc/apache2 \
- -v /home/chris/wt_leroy/web/log:/var/log/apache2 \
- -v /home/chris/wt_leroy/web/ssl:/etc/ssl \
+ -v /home/chris/wt-leroy/web/www:/var/www \
+ -v /home/chris/wt-leroy/web/etc:/etc/apache2 \
+ -v /home/chris/wt-leroy/web/log:/var/log/apache2 \
+ -v /home/chris/wt-leroy/web/ssl:/etc/ssl \
  chris/web
